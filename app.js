1449388a33980f07659c7d5f26d7f549a158cb6c
Jemgirl <dengratt@gmail.com>
2021-07-27 15:07:37 -0500

    conflict with socket in footer

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 // Foundation
+const path = require('path');
+const http = require('http');
 const express = require("express");
-const app = express();
+const socketio = require('socket.io');
 const mongoose = require("mongoose");
 const passport = require('passport');
 const LocalStrategy = require("passport-local");
 const passportLocalMongoose = require("passport-local-mongoose");
+const {userJoin, getCurrentUser, userLeave, getRoomUsers} = require('./utils/user.js');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 app.set("view engine", "ejs");  //adding this line makes it so we don't have to specify .ejs for file names
-app.use(express.static("public")); //connects express to the "public" folder where we made a css file
+app.use(express.static(path.join(__dirname, 'public'))); //connects express to the "public" folder where we made a css file
 const keys = require("./config/keys"); //links to private api key in config folder so no one has access. dev.js is added to gitignore
-
+               
 //Logger
 const logger = require("morgan");
 app.use(logger("dev") );
-
 
 mongoose.connect(keys.mongoURI,
   { //must use two lines of code below for mongoose to work
@@ -25,6 +31,9 @@ mongoose.connect(keys.mongoURI,
 app.use(express.urlencoded({extended: true}));
 
 let User = require("./models/user"); //connects to user file in models folder
+let Org = require("./models/orgs"); //connects to user file in models folder
+
+let formatMessage = require("./utils/messages"); //connects to user file in models folder
 
 let Orgs = require("./models/orgs"); //connects to org file in models folder
 
@@ -39,6 +48,45 @@ app.use(passport.session()); //allows access to session
 passport.use(new LocalStrategy(User.authenticate())); 
 passport.serializeUser(User.serializeUser()); //required to store data session
 passport.deserializeUser(User.deserializeUser()); //removes user session when they logout
+
+// image uploading packages:
+const multer = require('multer');
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+
+// this requires you to have '.env' file in root folder with API info
+cloudinary.config({ 
+  API_Environment_variable: process.env.CLOUDINARY_URL
+});
+
+const storage =  new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    // name of folder images will be stored in our Cloudinary account
+    folder: 'demo',
+  },
+});
+
+const parser = multer({ storage: storage });
+
+// TEMP route for temp "/api/images" form
+// NOTE: *Image is an example placeholder for your database collection. Substitute it for your own.
+// app.post('/api/images', parser.single("image"), (req, res) => {
+//   // console.log(req.file) // to see what is returned to you
+//   console.log("path to image: ", req.file.path) // this is the http address to the image
+//   const image = {};
+//   image.url = req.file.url;
+//   image.id = req.file.public_id;
+
+//   // THIS PART needs to send the img url to our mongodb to the new user's document:
+//   // ("Image" is placeholder for our db)
+
+//   // Image.create(image) // save image information in database
+//   //   .then(newImage => res.json(newImage))
+//   //   .catch(err => console.log(err));
+
+//   res.redirect('/')
+// });
 
 // Routes
 app.get("/", function(req, res) {  //links to home.ejs page
@@ -58,25 +106,20 @@ function getMatches(interestsArray) {
 
 }// end getMatches
 
-
-
 app.get("/results", isLoggedIn, function(req, res) { //isLoggedIn is middleware that only allows results page to show if you're logged in
 
   //The following console.log lines are to check/verify that the correct username and interests array are accessible via the request body.
   console.log("The username in question:  " + req.user.username);
   console.log("The user\'s interests are:" + req.user.interests);
 
-
   //The interests and username of the currently logged in user are stored in local variables.
   let interestsArray = req.user.interests;
   let username = req.user.username;
-
 
   console.log("What is the value of interestsArray:  " + interestsArray);
 
   //The user's interests (interestArray) is passed to a function called getMatches, where a query object is returned and stored in local variable results.
   let results = getMatches(interestsArray);
-
 
   //The returned query is first checked for any errors.  If there no errors, the code iterates through all the documents via a forEach loop.  For each document/record,
   //the field values are copied to a locally-defined object, which is then pushed into the matches array.  Finally, the matches array is passed to results.ejs.
@@ -99,6 +142,8 @@ app.get("/results", isLoggedIn, function(req, res) { //isLoggedIn is middleware 
         {
 	  var obj = {};
 	  var intersection = interestsArray.filter( x => elem.interests.includes(x) );
+
+
    
 	  obj.firstName = elem.firstName;
 	  obj.lastName = elem.lastName;
@@ -108,24 +153,95 @@ app.get("/results", isLoggedIn, function(req, res) { //isLoggedIn is middleware 
 	  obj.pic = elem.pic;
 	  obj.bio = elem.bio;
 	  obj.interests = intersection;
-
-
 	  matches.push(obj);
+
         }
 
-      });
-      console.log("Matches:  " + matches);
-      console.log("First document: " + matches[0].lastName);
-      res.render("results", {matches: matches});
-    }
+      }); // end forEach
+
+
+
+
+
+
+
+
+      //matches.forEach((elem) =>{
+      matches.forEach((elem, key, arr) =>{
+
+        console.log("\n\nFor the given match:  " + elem);
+
+        console.log("Declaring an empty array...");
+        var organizations = new Array();
+
+        //For each record, match up organizations to candidates based on interests.
+        console.log("Common interests...  " + elem.interests);
+
+
+
+        /*
+        getOrganizations(elem.interests) 
+        .then((element) =>{
+
+          element.forEach((org) =>{
+            console.log("Result of calling getOrganizations from the result route...  " + org.orgName);
+            organizations.push(org.orgName);
+          })// end forEach
+
+          console.log("Organizations Array...  " + organizations);
+	  elem.organizations = organizations;
+
+        })// end then
+        */
+
+
+
+
+
+
+
+        var results2 = getOrganizations(elem.interests);
+        results2.exec(function(err, doc){
+
+          if(err)
+          {
+            console.log(err);
+          }// end if
+          else
+          {
+            doc.forEach((org)=>{
+
+              organizations.push(org.orgName);
+            });// doc.forEach
+
+            elem.organizations = organizations;
+            console.log("The array of organizations..." + elem.organizations);
+             
+          }// end else
+
+
+	  if (Object.is(arr.length - 1, key)) {
+	    // execute last item logic
+	    //console.log(`Last callback call at index ${key} with value ${val}` ); 
+            res.render("results", {matches: matches});
+	  }
+        
+
+        });// end results2.exec
+
+
+      });// end matches.forEach
+
+
+
+      //res.render("results", {matches: matches});
+
+    }// end else
 
   });
   
+
 });// end /results
-
-
-  
-
 
 app.get("/signup", function(req, res) { //brings us to sign up page and profile questions
   res.render("signup");
@@ -147,8 +263,17 @@ app.get("/orgThanks", isLoggedIn, function(req, res) { //brings us to thank you 
   res.render("orgThanks");
 });
 
-//post route that handles logic for registering user & adding their info to database
-app.post("/signup", function(req, res) {
+app.get("/matchroom", isLoggedIn, function(req, res) { //brings us to sign in as user in a org chat room 
+  res.render("matchroom");
+});
+
+app.get("/chat", isLoggedIn, function(req, res) { //brings us to sign in as user in a org chat room 
+  res.render("chat");
+});
+
+// post route that handles logic for registering user & adding their info to database
+// parser handles image upload to Cloudinary
+app.post("/signup", parser.single("image"), function(req, res) {
   // passport stuff:
   console.log(req.body)
   
@@ -158,7 +283,10 @@ app.post("/signup", function(req, res) {
     firstName: req.body.fname,
     lastName: req.body.lname,
     email: req.body.email,
-    pic: req.body.avatar,
+    // pic: req.body.avatar,        // we don't need avatars anymore.
+    // PLACEHOLDER IMAGE as default:
+    // pic: 'https://res.cloudinary.com/dfg6bkjgg/image/upload/v1626496428/demo/i7cup02wozwmiytpwur9.jpg', 
+    pic: req.file.path, 
     gender: req.body.gender,
     ageRange: req.body.age,
     bio: req.body.bio,
@@ -172,8 +300,21 @@ app.post("/signup", function(req, res) {
         return res.render("signup")
       } else {
         passport.authenticate("local")(req, res, function() {
+          console.log("new user info: ", newUser) // to see if image upload address is included correctly
           res.redirect("/dashboard");
         });
+
+        // upload image to cloudinary *after* user added to db:
+        // console.log("path to image: ", req.file.path) // this is the http address to the image
+        // const image = {};
+        // add these to user db to store image url and id 
+        // first have defaults for these in the db document; then we will update with this info:
+        // image.url = req.file.url;
+        // image.id = req.file.public_id;'
+
+        // // this is not working:
+        // User.updateOne({ firstName: `"${newUser.firstName}"` }, { pic: `"${req.file.path}"`});
+        
       }
   })
 });
@@ -258,6 +399,52 @@ function isLoggedIn(req, res, next) {
   }
   res.redirect('/login');
 }// idAuthenticated is a built in passport method, checks to see if user is logged in, next( ) tells it to move to next piece of code
+
+const botName = 'ChatCord Bot';
+
+// Run when client connects
+io.on('connection', socket => {
+  socket.on('joinRoom', ({ username, room}) => {
+      const user = userJoin(socket.id, username, room);
+
+      socket.join(user.room);
+
+      // Welcome current user
+      socket.emit('message', formatMessage(botName, 'Welcome the ChatCord!'));
+
+      // Broadcast when a user connnects
+      socket.broadcast
+      .to(user.room)
+      .emit('message', formatMessage(botName, `${user.username} has joined the chat`));
+
+      // Send users and room info
+      io.to(user.room).emit('roomUser', {
+          room: user.room,
+          users: getRoomUsers(user.room)
+      });
+  });
+
+      // Listen to chatMessage
+      socket.on('chatMessage', msg => {
+      const user = getCurrentUser(socket.id);
+
+      io.to(user.room).emit('message', formatMessage(user.username, msg));
+  });
+
+  // Runs when client disconnects
+  socket.on('disconnect', () => {
+      const user = userLeave(socket.id);
+
+      io.to(user.room).emit('message', formatMessage(botName, `${user.username} has left the chat`));
+
+      // Send users and room info
+      io.to(user.room).emit('roomUser', {
+          room: user.room,
+          users: getRoomUsers(user.room)
+      });
+  });
+});
+
 
 // Listener
 const port = process.env.PORT || 3000; // this says run whatever port if 3000 is not available
